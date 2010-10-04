e36ee6597c792402d7b0bc0e26e0d4b6cc13393d
hieuhoang1972 <hieuhoang1972@1f5c12ca-751b-0410-a591-d2e778427230>
2010-10-04 08:33:12 +0000

    detab test
    git-svn-id: https://mosesdecoder.svn.sourceforge.net/svnroot/mosesdecoder/branches/hieu_ftemplate@3603 1f5c12ca-751b-0410-a591-d2e778427230

--- moses/src/InputType.h
+++ moses/src/InputType.h
@@ -36,68 +36,68 @@ class TranslationOptionCollection;
 class InputType 
 {
 protected:
-	long m_translationId; 	//< contiguous Id
-	bool m_hasMetaData;
-	long m_segId;
+  long m_translationId;   //< contiguous Id
+  bool m_hasMetaData;
+  long m_segId;
  
 public:
 
-	InputType(long translationId = 0);
-	virtual ~InputType();
+  InputType(long translationId = 0);
+  virtual ~InputType();
 
-	virtual InputTypeEnum GetType() const = 0;
+  virtual InputTypeEnum GetType() const = 0;
 
-	long GetTranslationId() const
-	{
-		return m_translationId;
-	}
-	void SetTranslationId(long translationId)
-	{
-		m_translationId = translationId;
-	}
-	//! returns the number of words moved
-	virtual int ComputeDistortionDistance(const WordsRange& prev, const WordsRange& current) const;
+  long GetTranslationId() const
+  {
+    return m_translationId;
+  }
+  void SetTranslationId(long translationId)
+  {
+    m_translationId = translationId;
+  }
+  //! returns the number of words moved
+  virtual int ComputeDistortionDistance(const WordsRange& prev, const WordsRange& current) const;
 
   //! In a word lattice, tells you if there's a path from node start to node end
-	virtual bool CanIGetFromAToB(size_t start, size_t end) const;
-	
+  virtual bool CanIGetFromAToB(size_t start, size_t end) const;
+  
   //! is there a path covering [range] (lattice only, otherwise true)
-	inline bool IsCoveragePossible(const WordsRange& range) const
-	{
-		return CanIGetFromAToB(range.GetStartPos(), range.GetEndPos() + 1);
-	}
+  inline bool IsCoveragePossible(const WordsRange& range) const
+  {
+    return CanIGetFromAToB(range.GetStartPos(), range.GetEndPos() + 1);
+  }
 
   //! In a word lattice, you can't always get from node A to node B
-	inline bool IsExtensionPossible(const WordsRange& prev, const WordsRange& current) const
-	{
-		//  return ComputeDistortionDistance(prev, current) < 100000;
-		size_t t = prev.GetEndPos()+1;
-		size_t l = current.GetEndPos()+1;
-		size_t r = l;
-		if (l<t) { r = t; } else { l = t; }
-		return CanIGetFromAToB(l,r);
-	}							
+  inline bool IsExtensionPossible(const WordsRange& prev, const WordsRange& current) const
+  {
+    //  return ComputeDistortionDistance(prev, current) < 100000;
+    size_t t = prev.GetEndPos()+1;
+    size_t l = current.GetEndPos()+1;
+    size_t r = l;
+    if (l<t) { r = t; } else { l = t; }
+    return CanIGetFromAToB(l,r);
+  }             
 
-	//! number of words in this sentence/confusion network
-	virtual size_t GetSize() const =0;
+  //! number of words in this sentence/confusion network
+  virtual size_t GetSize() const =0;
 
-	//! populate this InputType with data from in stream
-	virtual int Read(std::istream& in,const std::vector<FactorType>& factorOrder) =0;
-	
-	//! Output debugging info to stream out
-	virtual void Print(std::ostream&) const =0;
+  //! populate this InputType with data from in stream
+  virtual int Read(std::istream& in,const std::vector<FactorType>& factorOrder) =0;
+  
+  //! Output debugging info to stream out
+  virtual void Print(std::ostream&) const =0;
 
-	//! create trans options specific to this InputType
-	virtual TranslationOptionCollection* CreateTranslationOptionCollection() const=0;
+  //! create trans options specific to this InputType
+  virtual TranslationOptionCollection* CreateTranslationOptionCollection() const=0;
 
-	//! return substring. Only valid for Sentence class. TODO - get rid of this fn
-	virtual Phrase GetSubString(const WordsRange&) const =0;
+  //! return substring. Only valid for Sentence class. TODO - get rid of this fn
+  virtual Phrase GetSubString(const WordsRange&) const =0;
 
-	//! return substring at a particular position. Only valid for Sentence class. TODO - get rid of this fn
-	virtual const Word& GetWord(size_t pos) const=0;
+  //! return substring at a particular position. Only valid for Sentence class. TODO - get rid of this fn
+  virtual const Word& GetWord(size_t pos) const=0;
 
-	TO_STRING();
-	
+  TO_STRING();
+  
 };
 
 std::ostream& operator<<(std::ostream&,InputType const&);
