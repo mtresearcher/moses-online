e01c306de1a60cc6b1742ce11479409d41151080
Rico Sennrich <rico.sennrich@gmx.ch>
2012-11-15 15:53:02 +0100

    sample client for mosesserver

---
+++ contrib/server/client_multimodel.py
@@ -0,0 +1,53 @@
+# -*- coding: utf-8 -*-
+
+#
+# Sample python client. Additionally to basic functionality, shows how translation model weights can be provided to multimodel phrase table type,
+# and how translation model weights can be optimized on tuning set of phrase pairs
+#
+
+import sys
+import gzip
+
+if sys.version_info < (3, 0):
+    import xmlrpclib
+else:
+    import xmlrpc.client as xmlrpclib
+
+
+def translate(input_object, server, weights=None):
+
+    for line in input_object:
+        params = {}
+        params['text'] = line
+        if weights:
+            params['weight-t-multimodel'] = weights
+
+        print server.translate(params)
+
+
+def optimize(phrase_pairs, server):
+
+    params = {}
+    params['phrase_pairs'] = phrase_pairs
+    weights = server.optimize(params)
+    sys.stderr.write(str(weights + '\n'))
+    return weights
+
+
+def read_phrase_pairs(input_object):
+
+    pairs = []
+    for line in input_object:
+        line = line.split(' ||| ')
+        pairs.append((line[0],line[1]))
+    return pairs
+
+
+if __name__ == '__main__':
+    url = "http://localhost:8111/RPC2"
+    server = xmlrpclib.ServerProxy(url)
+
+    phrase_pairs = read_phrase_pairs(gzip.open('/path/to/moses-regression-tests/models/multimodel/extract.sorted.gz'))
+    weights = optimize(phrase_pairs, server)
+
+    translate(sys.stdin, server, weights)
