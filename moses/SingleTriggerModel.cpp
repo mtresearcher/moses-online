53a98346eaaecd4baa23015d48f9d3e456a73534
Prashant Mathur <pm.prashant@gmail.com>
2013-08-13 17:27:23 +0200

    starting to add word trigger model

---
+++ moses/SingleTriggerModel.cpp
@@ -0,0 +1,17 @@
+/* 
+ * File:   SingleTriggerModel.cpp
+ * Author: pmathur
+ * 
+ * Created on August 13, 2013, 5:21 PM
+ */
+
+#include "SingleTriggerModel.h"
+
+using namespace Moses;
+
+SingleTriggerModel::SingleTriggerModel() {
+}
+
+SingleTriggerModel::~SingleTriggerModel() {
+}
+
---
+++ moses/SingleTriggerModel.h
@@ -0,0 +1,23 @@
+/* 
+ * File:   SingleTriggerModel.h
+ * Author: pmathur
+ *
+ * Created on August 13, 2013, 5:21 PM
+ */
+
+#ifndef SINGLETRIGGERMODEL_H
+#define	SINGLETRIGGERMODEL_H
+
+#include "FeatureFunction.h"
+
+namespace Moses {
+class SingleTriggerModel : public StatefulFeatureFunction {
+public:
+    SingleTriggerModel();
+    virtual ~SingleTriggerModel();
+private:
+
+};
+}
+#endif	/* SINGLETRIGGERMODEL_H */
+
