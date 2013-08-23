65a61e4cca3ed5f203a061078f8da88aa79382f9
Prashant Mathur <pm.prashant@gmail.com>
2013-08-23 15:51:26 +0200

    vFunctions added in STM

--- moses/SingleTriggerModel.cpp
+++ moses/SingleTriggerModel.cpp
@@ -9,9 +9,28 @@
 
 using namespace Moses;
 
-SingleTriggerModel::SingleTriggerModel() {
+SingleTriggerModel::SingleTriggerModel() : StatefulFeatureFunction("SingleTriggerModel",1) {
+    
 }
 
 SingleTriggerModel::~SingleTriggerModel() {
+}
+
+FFState* SingleTriggerModel::Evaluate(const Hypothesis& hypo,
+                                     const FFState* prev_state,
+                                     ScoreComponentCollection* out) const
+{
+  Scores score(GetNumScoreComponents(), 0);
+//  const LexicalReorderingState *prev = dynamic_cast<const LexicalReorderingState *>(prev_state);
+//  LexicalReorderingState *next_state = prev->Expand(hypo.GetTranslationOption(), score);
+
+  out->PlusEquals(this, score);
+
+  return next_state;
+}
+
+const FFState* SingleTriggerModel::EmptyHypothesisState(const InputType &input) const
+{
+  return ;
 }
 
--- moses/SingleTriggerModel.h
+++ moses/SingleTriggerModel.h
@@ -10,14 +10,24 @@
 
 #include "FeatureFunction.h"
 
-namespace Moses {
-class SingleTriggerModel : public StatefulFeatureFunction {
-public:
-    SingleTriggerModel();
-    virtual ~SingleTriggerModel();
-private:
+typedef string trigger;
+typedef string word;
 
-};
+namespace Moses {
+
+    class SingleTriggerModel : public StatefulFeatureFunction {
+    public:
+        SingleTriggerModel();
+        virtual ~SingleTriggerModel();
+        
+        FFState* Evaluate(const Hypothesis& hypo,
+                                     const FFState* prev_state,
+                                     ScoreComponentCollection* out) const;
+        const FFState* SingleTriggerModel::EmptyHypothesisState(const InputType &input) const;
+        
+    private:
+        std::map<word, trigger> m_w2t;
+    };
 }
 #endif	/* SINGLETRIGGERMODEL_H */
 
